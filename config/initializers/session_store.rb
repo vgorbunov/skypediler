28a2824e94f1f9b56ec721f86f538e04d1ffdaa3
Valery Gorbunov <val.gorbunov@gmail.com>
2012-02-11 14:11:34 +0200

    First commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dialer::Application.config.session_store :cookie_store, key: '_dialer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dialer::Application.config.session_store :active_record_store
